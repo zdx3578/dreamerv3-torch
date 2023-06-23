24d995d28e7840173cb6fd83c702fe14bef69e26
zdx <179363811@qq.com>
2023-06-23 18:22:35 +0800

    stpn temp v0.17 bs32

--- dreamer.py
+++ dreamer.py
@@ -373,15 +373,18 @@ def main(config):
     state = None
     while agent._step < config.steps:
         logger.write()
+
+        print("Start training.")
+        state = tools.simulate(agent, train_envs, config.eval_every, state=state)
+        torch.save(agent.state_dict(), logdir / "latest_model.pt")
+
         print("Start evaluation.")
         eval_policy = functools.partial(agent, training=False)
         tools.simulate(eval_policy, eval_envs, episodes=config.eval_episode_num)
         if config.video_pred_log:
             video_pred = agent._wm.video_pred(next(eval_dataset))
             logger.video("eval_openl", to_np(video_pred))
-        print("Start training.")
-        state = tools.simulate(agent, train_envs, config.eval_every, state=state)
-        torch.save(agent.state_dict(), logdir / "latest_model.pt")
+
     for env in train_envs + eval_envs:
         try:
             env.close()
